0f6d756c4422838a34e19f3c378c058e2da5e592
Aaron Rykhus <aprykhus@hotmail.com>
2021-04-28 19:25:48 -0500

    add files to repo

---
+++ verticalwide.ps1
@@ -0,0 +1,15 @@
+﻿function Format-Vertical($array)
+{
+    $cols = 2
+    $rows = [math]::Ceiling($array.Length/$cols)
+
+    for ($i=0;$i -lt $rows; $i++)
+    {
+        #ColumnA
+        $array[$i]
+        #ColumnB and if/then to handle odd number lengths
+        if ($i -ne $rows -or $length % 2 -eq 0) {
+            $array[$i+$rows]
+        }
+    }
+}
---
+++ vertwide.ps1
@@ -0,0 +1,13 @@
+﻿function Format-Vertical($length)
+{
+    $cols = 2
+    $rows = [math]::Ceiling($length/$cols)
+
+    for ($i=0;$i -lt $rows; $i++)
+    {
+        $i
+        if ($i -ne $rows -or $length % 2 -eq 0) {
+            $i+$rows
+        }
+    }
+}
